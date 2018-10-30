963326f01f26409a4ca5cd5637b829c25f2703de
Maier, Martin <martin.maier@saxsys.de>
2018-10-30 16:35:50 +0100

    #2 #3 enabled e2e test "should display a header "TODOs" and developed app to let it succeed

---
+++ src/app/todo/todo-list/list.component.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListComponent } from './list.component';
+
+describe('TodoListComponent', () => {
+  let component: ListComponent;
+  let fixture: ComponentFixture<ListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display a header TODOs', function () {
+    const headerElement: HTMLHeadElement = fixture.nativeElement.querySelector('h1');
+    expect(headerElement.textContent).toEqual('TODOs')
+  });
+});
